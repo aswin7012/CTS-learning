449a887731bbf27c5afc6fbe596fecfb0ef06196
ASHWIN <ashwin202507@gmail.com>
2025-06-21 09:44:30 +0000

    week-1 learning

---
+++ FactoryMethodPatternExample/News.java
@@ -0,0 +1,60 @@
+// No class should be public
+interface Document {
+    void open();
+}
+
+class WordDocument implements Document {
+    public void open() {
+        System.out.println("Opening Word document");
+    }
+}
+
+class PdfDocument implements Document {
+    public void open() {
+        System.out.println("Opening PDF document");
+    }
+}
+
+class ExcelDocument implements Document {
+    public void open() {
+        System.out.println("Opening Excel document.");
+    }
+}
+
+abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+class WordFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory extends DocumentFactory {
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+    }
+}
